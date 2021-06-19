3af438334e5a3d0582caf148e1e4df0480476b16
Jay Pitman <jakeseanp@gmail.com>
2021-06-19 20:14:13 +1000

    Pass params between screens

--- App.tsx
+++ App.tsx
@@ -1,28 +1,44 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import {
   NavigationContainer,
   StackActionHelpers,
 } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-const HomeScreen = ({ navigation }: { navigation: any }) => {
+const HomeScreen = ({ navigation, route }: { navigation: any; route: any }) => {
+  const selectedOption = route.params?.option;
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to details"
+      <Text>
+        {selectedOption
+          ? `You selected ${selectedOption}`
+          : "You haven't made a selection yet"}
+      </Text>
+      <TouchableOpacity
         onPress={() => navigation.navigate("Details")}
-      />
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Go to Details</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const DetailsScreen = () => {
+const DetailsScreen = ({ navigation }: { navigation: any }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
+      {["1", "2", "3"].map((option) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home", { option })}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>{option}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
@@ -50,5 +66,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    width: 100,
+    borderWidth: 1,
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  buttonText: {
+    color: "white",
   },
 });
